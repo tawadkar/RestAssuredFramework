84c0285754c98fbf4c44f7065ca05106bf8644b5
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2022-05-31 10:53:09 +0530

    Cucumber HTML Report Configuration

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features",glue ={"stepDefinitions"},tags = "@DeletePlace")
+@CucumberOptions(features="src/test/java/features", plugin="json:target/jsonReports/cucumber-report.json", glue ={"stepDefinitions"},tags = "@AddPlace")
 public class TestRunner {
 
 
