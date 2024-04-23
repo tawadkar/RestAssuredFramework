b6d8f04e270f99d6264a7f0b018361db5e9e5c1b
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2024-04-24 00:33:11 +0530

    Pojo Classes for Graphql Queries

---
+++ src/main/java/pojoClasses/queryVariable.java
@@ -0,0 +1,14 @@
+package pojoClasses;
+
+public class queryVariable {
+
+    private int limit;
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+}
--- src/test/java/resources/graphQL.java
+++ src/test/java/resources/graphQL.java
@@ -2,16 +2,36 @@ package resources;
 import io.cucumber.java.en.Given;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
+import pojoClasses.graphQLQuery;
+import pojoClasses.queryVariable;
 
 import static io.restassured.RestAssured.*;
 
 public class graphQL {
     public static void main (String [] args){
+        graphQLQuery query  =new graphQLQuery();
+        queryVariable variable = new queryVariable();
+        /*
+           limit:Int is considered in the query while execution but not limiting the output , need to check
+           Pojo Class for Setting Query and Variables is referred from NaveenLabs Youtube video
+        */
+        query.setQuery("query($limit:Int!){\n" +
+                "  allBooks(limit:$limit) {\n" +
+                "    id\n" +
+                "    title\n" +
+                "    desc\n" +
+                "    price\n" +
+                "    author\n" +
+                "  }\n" +
+                "}");
+        variable.setLimit(1);
+        query.setVariables(variable);
         String response = given().log().all().header("Content-Type","application/json")
                 /***
                  * String in the body is captured from Network Tab >>Payload >> View Source
                  */
-                .body("{\"query\":\"{\\n  allBooks {\\n    id\\n    title\\n    desc\\n    price\\n    author\\n  }\\n}\"}")
+               // .body("{\"query\":\"{\\n  allBooks {\\n    id\\n    title\\n    desc\\n    price\\n    author\\n  }\\n}\"}")
+                .body(query)
                 .when()
                 .post("http://localhost:8009/graphql")
                 .then().extract().response().asString();
@@ -51,6 +71,7 @@ public class graphQL {
 }
       Practice GraphQL Server: https://graphql.org/swapi-graphql
        */
+
 
 
     }
